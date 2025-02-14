35220e6bf40077ab72a6362f452dd91b275f7898
Rocio Dorado <rocio_dorado12@hotmail.com>
2025-02-13 21:46:37 -0300

    rama animaciones creada

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./styles/Inicio.css";
@@ -10,7 +11,7 @@ import ContactUs from "./components/views/ContactUs";
 import Servicios from "./components/views/Servicios";
 import Ofrecemos from "./components/views/Ofrecemos";
 import Elegirnos from "./components/views/Elegirnos";
-import { useRef } from "react";
+import Proyectos from "./components/views/Proyectos";
 
 function App() {
   const contactRef = useRef(null); // Referencia a la sección de contacto
@@ -19,14 +20,13 @@ function App() {
     <>
       <Menu contactRef={contactRef} />
       <section className="mainSection">
-        <Inicio contactRef={contactRef} />
-        <Servicios />
-        <Ofrecemos />
-        <Elegirnos />
-        <About />
-        <div ref={contactRef}>
-          <ContactUs />
-        </div>
+        <Inicio></Inicio>
+        <Servicios></Servicios>
+        <Ofrecemos></Ofrecemos>
+        <Elegirnos></Elegirnos>
+        <Proyectos></Proyectos>
+        <About></About>
+        <ContactUs></ContactUs>
       </section>
       <Footer />
     </>
